d522d7e7e61537b88c3de352975e0d21ae2b2a74
Paulius Pilkauskas <talonas@paulius-pilkauskass-macbook-pro.local>
2010-09-12 18:19:30 +0300

    some comments

--- main.c
+++ main.c
@@ -9,6 +9,9 @@ main(int argc, char **argv)
 	int i;
 	int retval;
 
+	/**
+	 * Initialize protection
+	 */
 	retval = protection_get_license();
 	if (retval != 0)
 	{
@@ -16,11 +19,17 @@ main(int argc, char **argv)
 		return -1;
 	}
 	
+	/**
+	 * Say that program is running
+	 */
 	for (i = 0; i < 20; i++)
 	{
 		protection_check();
 	}
 
+	/**
+	 * Deinit
+	 */
 	protection_free();
 
 	return 0;
