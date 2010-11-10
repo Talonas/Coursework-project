0862c088c3177ca7dac6174892bd464f599c1669
Paulius Pilkauskas <pauliusp@neurotechnology.com>
2010-11-10 09:19:44 +0200

    remove files

--- catmap.h
+++
@@ -1,8 +0,0 @@
-#ifndef _CAT_MAP_H
-#define _CAT_MAP_H
-
-void cat_map_matrix_init(void);
-void cat_map_transform(void);
-void cat_map_print(void);
-
-#endif /* _CAT_MAP_H */
--- protection.h
+++
@@ -1,8 +0,0 @@
-#ifndef _PROTECTION_H
-#define _PROTECTION_H
-
-void init_license(void);
-int get_license(void);
-void protection_free(void);
-
-#endif /* _PROTECTION_H */
