42703f75cdcb9098792a21c7020fff8dfcf0a0e3
Paulius Pilkauskas <pauliusp@neurotechnology.com>
2010-11-10 14:54:50 +0200

    UPDATE: removed some includes from bakers map

--- prot_bakers_map/bakers_map.c
+++ prot_bakers_map/bakers_map.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <string.h>
 
 #include "bakers_map.h"
 
@@ -22,5 +23,42 @@ bakers_map_matrix_init(void)
 void
 bakers_map_transform(void)
 {
+	int i, j;
+	int row, col;
+	int temp_matrix[size][size];
+	int temp_matrix_2[size][size];
 
+	memcpy(temp_matrix, matrix, sizeof(temp_matrix));
+
+	row = 0;
+	for (i = 0; i < size; i++)
+	{
+		col = size - 1;
+		for (j = 0; j < size; j++)
+		{
+			temp_matrix_2[row][col] = temp_matrix[j][i];
+			col--;
+		}
+		row++;
+	}
+	
+	row = 0;
+	for (i = size/2; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			matrix[row][j] = temp_matrix_2[i][j];
+		}
+		row++;
+	}
+
+	for (i = 0; i < size/2; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			matrix[row][j] = temp_matrix_2[i][j];
+		}
+		row++;
+	}
 }
+
